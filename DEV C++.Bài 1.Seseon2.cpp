ba16d58bbaad681607703d1bb30480152ac42a4a
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2024-11-21 08:38:42 +0700

    Bài tập về nhà 1

---
+++ DEV C++.Bài 1.Seseon2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int SoHocSinh;//kieu so nguyen
+    float DiemTB; //kieu so thuc
+    char TenSV; //kieu ki tu
+    double MaSV;//kieu so thuc nhung lon hon
+    return 0;
+}
